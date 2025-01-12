5ba5d1eadacfc6aaba07c3ed99932e86b6625aa5
puligurubrahmam <147025676+puligurubrahmam@users.noreply.github.com>
2025-01-12 11:15:00 +0530

    Update index.js

--- src/components/Question/index.js
+++ src/components/Question/index.js
@@ -1,70 +1,77 @@
-import './index.css'
-import {Component} from 'react'
+import './index.css';
+import { Component } from 'react';
 
-class Question extends Component
-{
-    state={questionId:null,display:false}
-    render()
-    {
-        const {questionObject} = this.props;
-        return (
-            <div className='questionContainer'>
-                <h1 className='question-heading'>{questionObject.props?.[0]?.question}</h1>
-                <ul className='question-ul'>
-                    {
-                        (questionObject.props?.[0]?.points!==undefined)&&questionObject.props?.[0]?.points.map((point)=>
-                        {
-                            return (
-                                <li className='question-ul-li'>
-                                    <img src={`https://www.medibuddy.in${point.img}`} alt={point.img}/>
-                                    <p className='point-para'>{point.pnt}</p>
-                                </li>
-                            )
-                        })
-                    }
-                </ul>
-            <h1 className='question-heading'>{questionObject.title}</h1>
-            <div className='question-container'>
-            <ul className='frequent-question-ul'>
-                {
-                    (questionObject.props!==undefined)&&questionObject.props.map((item,index)=>
-                    {
-                        if(index>0)
-                        {
-                            return (
-                                <li>
-                                    <div className='question-top'>
-                                        <h1 className='question-head'>{item.question}</h1>
-                                        <button onClick={()=>{this.setState((prevState)=>{return {questionId:item.id,display:!prevState.display}})}} className='circle'>
-                                            {(this.state.display)?'-':'+'}
-                                        </button>
-                                    </div>
-                                    {
-                                    (item.id===this.state.questionId&&this.state.display)&&(<div>
-                                    <p className='pnt-txt'>{item.answer}</p>
-                                    <ul className='point-ul'>
-                                        {
-                                            item.points.map((point)=>
-                                            {
-                                                return (
-                                                    <li>
-                                                        <p className='pnt-txt'>{point.pnt}</p>
-                                                    </li>
-                                                )
-                                            })
-                                        }
-                                    </ul>
-                                    </div>)
-                                    }
-                                </li>
-                            )
-                        }
-                    })
-                }
-            </ul>
-            </div>
-            </div>
-        )
-    }
+class Question extends Component {
+  state = { questionId: null, display: null };
+
+  handleToggle = (id) => {
+    
+    this.setState(prevState => ({
+      questionId: id,
+      display: prevState.display === id ? null : id 
+    }));
+  }
+
+  render() {
+    const { questionObject } = this.props;
+
+    if (!questionObject || !questionObject.props) return null; 
+
+    return (
+      <div className='questionContainer'>
+        <h1 className='question-heading'>{questionObject.props?.[0]?.question}</h1>
+
+        <ul className='question-ul'>
+          {(questionObject.props?.[0]?.points?.length > 0) && questionObject.props[0].points.map((point, index) => (
+            <li key={index} className='question-ul-li'>
+              <img src={`https://www.medibuddy.in${point.img}`} alt={point.img} />
+              <p className='point-para'>{point.pnt}</p>
+            </li>
+          ))}
+        </ul>
+
+        <h1 className='question-heading'>{questionObject.title}</h1>
+
+        <div className='question-container'>
+          <ul className='frequent-question-ul'>
+            {questionObject.props.map((item, index) => {
+              if (index > 0) {
+                return (
+                  <li key={item.id}>
+                    <div className='question-top'>
+                      <h1 className='question-head'>{item.question}</h1>
+                      <button
+                        onClick={() => this.handleToggle(item.id)}
+                        className='circle'
+                        aria-label={this.state.display === item.id ? 'Collapse answer' : 'Expand answer'}
+                      >
+                        {this.state.display === item.id ? '-' : '+'}
+                      </button>
+                    </div>
+
+                    
+                    {this.state.display === item.id && (
+                      <div>
+                        <p className='pnt-txt'>{item.answer}</p>
+                        <ul className='point-ul'>
+                          {item.points?.map((point, idx) => (
+                            <li key={idx}>
+                              <p className='pnt-txt'>{point.pnt}</p>
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
+                    )}
+                  </li>
+                );
+              }
+              return null; 
+            })}
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
+
 export default Question;
